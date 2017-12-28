f1c9a6b9035a08f512d55467891a060290a08625
Sridhar S <sridhars@syncfusion.com>
2017-12-28 18:04:47 +0530

    fixed server closing error

--- src/bot/bot-brain.js
+++ src/bot/bot-brain.js
@@ -3,7 +3,7 @@ var classifier = new BrainJSClassifier();
 var http = require('http');
 var url = require('url');
 
-classifier.addDocument('my unit-tests failed.', 'software');
+classifier.addDocument('my unit-tests failed.', {});
 classifier.addDocument('tried the program, but it was buggy.', 'software');
 classifier.addDocument('tomorrow we will do standup.', 'meeting');
 classifier.addDocument('the drive has a 2TB capacity.', 'hardware');
@@ -12,12 +12,12 @@ classifier.addDocument('can you play some new music?', 'music');
 
 classifier.train();
 
-
-
 http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/json' });
     var q = url.parse(req.url, true).query;
-    // var txt = q.year + " " + q.month;
-
-    res.end(classifier.classify(q.chat));
-}).listen(8000)
+    if (q.chat) {
+        res.end(classifier.classify(q.chat));
+    } else {
+        res.end({});
+    }
+}).listen(8000);
