c85d8fbb66cab3ec8e33519fef47987c16cd82cb
manisankardurai <manisankar.durai@syncfusion.com>
2017-12-29 09:36:22 +0530

    Update Scriptfile.js

--- src/scripts/Scriptfile.js
+++ src/scripts/Scriptfile.js
@@ -10,7 +10,6 @@ $(function () {
                 $('.messages').append($message);
                 return setTimeout(function () {
                     $("body .chat_window .messages").css("background", "linear-gradient(to right, lightgreen, pink)");
-
                     return $message.addClass('appeared');
                 }, 0);
             };
@@ -33,34 +32,38 @@ $(function () {
             $('.message_input').val('');
             $messages = $('.messages');
             message_side = message_side === 'left' ? 'right' : 'left';
+            var exp = /(\b(https?|ftp|file):\/\/([-A-Z0-9+&@#%?=~_|!:,.;]*)([-A-Z0-9+&@#%?\/=~_|!:,.;]*)[-A-Z0-9+&@#\/%=~_|])/ig;
+            var text =  text.replace(exp, "<a href='$1' target='_blank'>$3</a>");
             message = new Message({
                 text: text,
                 message_side: message_side
             });
+
             message.draw();
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
         };
         $('.send_message').click(function (e) {
             return sendMessage(getMessageText());
         });
-
+       
         $('.message_input').keyup(function (e) {
             if (e.which === 13) {
                 usertext = getMessageText();
                 sendMessage(getMessageText());
                 setTimeout(function () {
                     $.ajax({
-                        type: "GET",
-                        url: "http://localhost:8000/?chat=\"" + usertext + '"',
-                        dataType: "json",
-                        contentType: "application/json",
+                        url: 'http://localhost:8000/?chat=saa',
+                        dataType: 'json',
+                        type: 'GET',
                         success: function (data) {
-                            debugger;
                             sendMessage(data);
-                        }
+                            
+                        },
+                        error: function () {  },
+                       
+                    });
 
-                    })
-                }, 0)
+                }, 1000)
             }
         });
     });
